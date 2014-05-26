39633a6db0470a4da4d04711470b899c43a02f88
ThomasDebek <tom.debek@gmail.com>
2014-05-26 22:18:01 +0200

    Dodanie api :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppGeocoder::Application.config.secret_key_base = '54b5613d3247525b1f5c9eb1ad3573db83a29b0d83d546c4a5a3ec362bb111d0f9994f47efa426595dd47190a28491821c7b2caed3b42576564ecfc6527e37fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppGeocoder::Application.config.session_store :cookie_store, key: '_app_geocoder_session'
